44026bece052954a382fc1561f4514ad38f3b282
zereight <42544600+zereight@users.noreply.github.com>
2019-01-29 00:02:06 +0900

    7. Make a some Schemas (1)
    1. Make api folder and create graphql, resolver files.
    2. For ready to deliver to app's GraphqlServer, we'll use graphql-tools and merge-graohql0schemas.

---
+++ src/api/Hello/sayBye/sayBye.resolvers.ts
@@ -0,0 +1,7 @@
+const resovlers = {
+    Query : {
+        sayBye: (): string => {return "say Bye"}
+    }
+};
+
+export default resovlers;
---
+++ src/api/Hello/sayHello/sayHello.resolvers.ts
@@ -0,0 +1,7 @@
+const resovlers = {
+    Query : {
+        sayHello: (): string => {return "say Hello"}
+    }
+};
+
+export default resovlers;
