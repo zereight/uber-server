0647176f08aeccf4dea2656498e81c1bcf805a69
zereight <42544600+zereight@users.noreply.github.com>
2019-01-28 23:29:20 +0900

    6. Make Server without Schema
    We made a graphql server which has Option, handle fhunction, but we don't have Schema yet.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("work!")
+import app from "./app"
+import { Options } from "graphql-yoga";
+
+const PORT : number | string = process.env.PORT || 4000;
+const GRAPHQL_ENDPOINT : string = "/graphql";
+const PLAYGROUND : string = "/playground";
+
+
+const appOptions : Options = {
+    port: PORT,
+    endpoint: GRAPHQL_ENDPOINT,
+    playground: PLAYGROUND
+};
+
+const print_strat  = () : void => {
+    console.log(`This server started on PORT : ${PORT}`);
+}
+
+
+app.start(appOptions, print_strat);
