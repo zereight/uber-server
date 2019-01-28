d0ac87bbdf4d089355fd422554ac4af6a7fa2cc0
zereight <42544600+zereight@users.noreply.github.com>
2019-01-29 00:26:34 +0900

    8. Make a Schema (2)
    We merged .graphql and .resolvers.ts files, also tranformed to GraphQLSchema.
    
    It is offered to app's GraphQLServer.
    
    We can check on online "localhost:4000/playground".

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,14 @@ import cors from "cors";
 import logger from "morgan";
 import helmet from "helmet";
 
+import schema from "./schema"
+
 class App{
     public app : GraphQLServer;
 
     constructor(){
         this.app = new GraphQLServer({
-            
+            schema
         });
 
         this.middlewares();
