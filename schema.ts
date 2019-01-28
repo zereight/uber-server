79d774b42aca8dcfac27e839c39a3170e4b29b6b
zereight <42544600+zereight@users.noreply.github.com>
2019-01-29 00:26:34 +0900

    no.s

--- schema.ts
+++
@@ -1,5 +0,0 @@
-import { GraphQLSchema } from "graphql";
-import { makeExecutableSchema } from "graphql-tools";
-import { fileLoader, mergeResolvers, mergeTypes } from "merge-graphql-schemas";
-import { path } from "path";
-
